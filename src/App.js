443e7546cbec5bfc12a4d5a9e9e7756aa33cee00
Uriel Cristino <urielcris.97@gmail.com>
2021-09-20 18:08:41 -0500

    Consultando la API y leyendo sus valores

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 
 // Components
 import Form from "./components/Form";
@@ -8,6 +8,21 @@ function App() {
 
   // Definimos la categoria y noticias
   const [categoria, setCategoria] = useState('')
+  const [noticias, setNoticias] = useState([]);
+
+  // Detecta cuando cambie la categoria
+  useEffect(() => {
+    const api_key = '3c5fe726a6ce40cab0981c1931b8563a'
+    const consultarAPI = async () => {
+      const URL_API = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${api_key}`;
+
+      const response= await fetch(URL_API);
+      const data = await response.json();
+      setNoticias(data.articles);
+    }
+
+    consultarAPI()
+  }, [categoria])
 
   return (
     <Fragment>
