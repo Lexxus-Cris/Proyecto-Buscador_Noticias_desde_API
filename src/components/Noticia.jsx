0639bcad3161c96e18e04b13a07c3e9fcd989273
Uriel Cristino <urielcris.97@gmail.com>
2021-09-20 23:52:44 -0500

    Mostrando las noticias en la interface

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
-const Noticia = () => {
+const Noticia = ({noticia}) => {
+
+   // Extraer los datos de la noticia.
+   const { urlToImage, url, title, description, source } = noticia;
+
+   // Revisar si una imagen tiene contenido
+   const image = (urlToImage) ? 
+      <div className="card-image">
+         <img src={urlToImage} alt={title} />
+         <span className="card-title">{source.name}</span>
+      </div>
+   : null;
+
    return (
-      <div>
-         Noticia
+      <div className="col s12 m6 l4">
+         <div className="card">
+
+            {image}
+
+            <div className="card-content">
+               <h3
+                  id="Notice-title"
+               >{title}</h3>
+               <p>{description}</p>
+            </div>
+
+            <div className="card-action">
+               <a
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="waves-effect waves-light btn"
+               >Ver Noticia Completa</a>
+            </div>
+         </div>
       </div>
    )
 }
